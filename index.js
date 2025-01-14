38b42273a563e490179b4eb0c875d0ecac98c86b
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-01-14 10:50:52 +0600

    added admin verification middleware

--- index.js
+++ index.js
@@ -52,7 +52,19 @@ async function run() {
           return res.status(401).send({ message: "unauthorized access" });
         }
         req.decoded = decoded;
+        next();
       });
+    };
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
     };
 
     // user related api

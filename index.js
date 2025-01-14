f17598f6f226462b0f52fc41cb39a7200b8eb710
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-01-14 11:30:29 +0600

    Added post route for add user

--- index.js
+++ index.js
@@ -90,6 +90,21 @@ async function run() {
       res.send({ admin });
     });
 
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      // insert email if user doesn't exists:
+      // you can do this many ways (1. email unique, 2. upsert 3. simple checking )
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
